126959e844525205b8e72d3850a068de4b633436
Zheng Yang <zheng@ip-192-168-128-104.us-west-2.compute.internal>
2017-07-27 14:57:22 -0700

    add centos7 and some simple tests

--- applatix/build-docker-build.sh
+++ applatix/build-docker-build.sh
@@ -2,3 +2,7 @@
 
 docker build -f Dockerfile.build.ubuntu-16.04 -t applatix/node-build-ubuntu-16.04 .
 docker push applatix/node-build-ubuntu-16.04
+
+docker build -f Dockerfile.build.centos7 -t applatix/node-build-centos7 .
+docker push applatix/node-build-centos7
+
